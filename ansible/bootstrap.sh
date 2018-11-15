3cddb338125f7d935c67dc0121ea7645335cb4d9
Nick Cross <ncross@redhat.com>
2018-11-15 12:12:36 +0000

    Remove sudo

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-su root
 dnf install -y git ansible
 git clone https://github.com/rnc/nicks-shell.git /tmp/nicks-shell
-
 echo -e "Now run\n\tcd /tmp/nicks-shell/ansible\n\tsudo ansible-playbook --ask-vault-pass -v playbook.yml"

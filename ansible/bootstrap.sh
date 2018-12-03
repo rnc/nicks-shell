5fe3ee870d9f9f98b4e3f30ee3ab9620cc7a4739
Nick Cross <ncross@redhat.com>
2018-12-03 10:16:58 +0000

    Improve documentation and add method to store vault password within wallet. Reuse bootstrap script to also run playbook

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-dnf install -y git ansible
-git clone https://github.com/rnc/nicks-shell.git /tmp/nicks-shell
-echo -e "Now run\n\tcd /tmp/nicks-shell/ansible\n\tsudo ansible-playbook --ask-vault-pass -v playbook.yml"
+if [ "$( rpm -q ansible git)" == "0" ]
+then
+    echo -e "\033[49;32;1mBootstraps to /tmp/nicks-shell...\033[0m"
+    dnf install -y ansible git
+    git clone https://github.com/rnc/nicks-shell.git /tmp/nicks-shell
+else
+    echo -e "\033[49;32;1mBootstrap already performed ; executing ansible using vault method...\033[0m"
+    if [ "`basename $(pwd)`" != "ansible" ]
+    then
+        echo "Run from ansible directory"
+        exit 1
+    fi
+
+    ansible-playbook -v playbook.yml -e "ansible_become_pass=$(kwallet-query -l kdewallet -f ksshaskpass -v -r '')"
+fi

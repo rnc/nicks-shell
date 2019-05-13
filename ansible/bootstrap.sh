208c72d588754e4b2e9e15afa219e2f97142e690
Nick Cross <ncross@redhat.com>
2019-05-13 12:44:15 +0100

    Minor fixes to bootstrap and ansible scripts when updating to F30

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -4,17 +4,19 @@ if [ "$( grep '^ID=fedora' /etc/os-release )" ]
 then
     PKGQ="rpm -q"
     PKGI="dnf"
-    EXTRA="ansible_become_pass=$(kwallet-query -l kdewallet -f ksshaskpass -v -r '')"
+    EXTRA="ansible_become_pass=$(kwallet-query -l kdewallet -f ksshaskpass -v -r '' ) "
 else
     PKGQ="dpkg-query -W"
     PKGI="apt-get"
     EXTRA=" "
 fi
 
-if [ "$( $PKGQ ansible git)" == "0" ]
+$PKGQ ansible git > /dev/null
+if [ "$?" == "2" ]
 then
     echo -e "\033[49;32;1mBootstraps to /tmp/nicks-shell...\033[0m"
     $PKGI install -y ansible git
+    [[ "$?" == 1 ]] && exit 1
     git clone https://github.com/rnc/nicks-shell.git /tmp/nicks-shell
 else
     echo -e "\033[49;32;1mBootstrap already performed ; executing ansible using vault method...\033[0m"

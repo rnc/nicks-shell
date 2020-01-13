f47cba78a4a5031ce18443866b63e42b42c29203
Nick Cross <ncross@redhat.com>
2020-01-13 13:23:35 +0000

    Reorder

--- misc/user.js
+++ misc/user.js
@@ -1,14 +1,10 @@
 
 
-
 /* 0000: disable about:config warning
  * The XUL version can still be accessed in FF71+ @ chrome://global/content/config.xul
  * and in FF73+ @ chrome://global/content/config.xhtml ***/
 user_pref("general.warnOnAboutConfig", false); // for the XUL version
 user_pref("browser.aboutConfig.showWarning", false); // for the new HTML version [FF71+]
-
-// https://support.mozilla.org/bm/questions/1241129
-user_pref("browser.ctrlTab.recentlyUsedOrder", false);
 
 // https://github.com/Aris-t2/CustomCSSforFx
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
@@ -20,6 +16,9 @@ user_pref("toolkit.telemetry.bhrPing.enabled", false);
 user_pref("toolkit.telemetry.newProfilePing.enabled", false);
 user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
 
+// https://support.mozilla.org/bm/questions/1241129
+user_pref("browser.ctrlTab.recentlyUsedOrder", false);
+
 // Always show download button
 user_pref("browser.download.autohideButton", false);
 
@@ -29,12 +28,10 @@ user_pref("browser.download.useDownloadDir", false);
 // Session store
 user_pref("browser.sessionstore.warnOnQuit", true);
 
+// Hide recommended extensions/addons
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 
 // Privacy
 user_pref("privacy.trackingprotection.enabled", true);
 user_pref("privacy.trackingprotection.socialtracking.enabled", true);
 user_pref("privacy.donottrackheader.enabled", true);
-
-
-// Hide recommended extensions/addons
-user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);

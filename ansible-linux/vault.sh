dba09bacaaf807f36a5928731e437a9de090127f
Nick Cross <gitkraken@goots.org>
2020-06-30 10:38:58 +0100

    Corrections for F32. Added Birdtray

--- ansible-linux/vault.sh
+++ ansible-linux/vault.sh
@@ -2,7 +2,11 @@
 
 if [ "$( grep '^ID=fedora' /etc/os-release )" ]
 then
-    kwallet-query -r ansible-vault kdewallet
+    kwallet-query -r ansible-vault kdewallet 2> /dev/null
+    if [ $? != 0 ]
+    then
+       read -p "Enter password: "
+    fi
 else
     read -p "Enter password: "
 fi
